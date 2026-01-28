024bd90a893089c1340150681bd9f7d82e2c7d58
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2026-01-29 04:41:45 +0800

    Finalized version of website

--- app/page.jsx
+++ app/page.jsx
@@ -14,12 +14,12 @@ export default function Home() {
       <h2 className="flex justify-center font-extrabold text-6xl white-text-shadow ">Paul Josef P. Agbuya</h2>
       <h2 className="flex justify-center font-extrabold text-1xl sub-h">Graduate in BS Computer Science of De La Salle University</h2>
      <div className="flex justify-center gap-4 my-4">
-      <a href="https://github.com/your-username" target="_blank">
+      <a href="https://github.com/your-username" target="_blank" className="hover:scale-110 transition-transform duration-300">
           <Image src={"https://img.shields.io/badge/GitHub-181717?style=for-the-badge&logo=github&logoColor=white"} alt="GitHub"
               width={200} height={100}/>
       </a>
 
-      <a href="https://linkedin.com/in/your-id" target="_blank">
+      <a href="https://linkedin.com/in/your-id" target="_blank" className="hover:scale-110 transition-transform duration-300">
           <Image src={"https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"}  alt="LinkedIn"
               width={200} height={100}/>
       </a>
